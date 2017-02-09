93316c3c5cc342ab5a9e8b953867882fb0884040
Elizabeth Gulsby <elizabeth.gulsby@gmail.com>
2017-02-09 17:00:33 -0500

    init commit

---
+++ back-end/routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: '127.0.0.1',
+	user: 'x',
+	password: 'x',
+	database: 'students'
+})
+
+connection.connect();
+
+router.get('/getStudents', (req, res, next) =>{
+	connection.query('SELECT * from students', (error, results, fields) => {
+		if (error) throw error;
+		res.json(results);
+	});
+
+});
+
+router.post('/addStudent', (req, res, next) => {
+	var studentToAdd = req.body.name;
+	connection.query('insert into students (name) values(?)', [studentToAdd], (error, results, fields) =>{
+		if (error) throw error;
+		connection.query('SELECT * from students', (error, results, fields) => {
+			if (error) throw error;
+			res.json(results);
+		});
+	})
+	// res.json([studentToAdd]);
+});
+
+module.exports = router;
